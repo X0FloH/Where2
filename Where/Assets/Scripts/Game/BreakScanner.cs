749a311311bdc491c16de9057393cab52bf0e589
X0FloH <oscar@rwjones.info>
2018-03-29 14:01:26 +0100

    Added Scanner Anim

---
+++ Where/Assets/Scripts/Game/BreakScanner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakScanner : MonoBehaviour {
+
+    public float xTarget;
+    public GameObject player;
+    public GameObject fallCam;
+    public Finished finishAnim;
+
+    public List<GameObject> lights = new List<GameObject>();
+
+    public bool done;
+
+    private void Update()
+    {
+        if (!done)
+        {
+            if(player.transform.position.x <= xTarget)
+            {
+                done = true;
+                fallCam.SetActive(true);
+                player.SetActive(false);
+                InvokeRepeating("Checking", 0.01f, Time.deltaTime);
+            }
+        }
+    }
+
+    void Checking()
+    {
+        if (finishAnim.finished)
+        {
+            fallCam.SetActive(false);
+            player.SetActive(true);
+        }
+    }
+}
